c72d6b1374ea52baa79908d4b66c9f267d033ccb
Maxim Grigorev <maxkraft06@gmail.com>
2025-12-07 19:42:53 +0000

    add my libraries

---
+++ lab4_var5/my_libs/librealize1.c
@@ -0,0 +1,17 @@
+#include <math.h>
+
+double SinIntegral(double A, double B, double e) {
+    double sum = 0.0;
+    double x = A + e / 2.0;
+    
+    while (x < B) {
+        sum += sin(x) * e;
+        x += e;
+    }
+
+    return sum;
+}
+
+double E(double x) {
+    return pow(1 + 1 / x, x);
+}
---
+++ lab4_var5/my_libs/librealize2.c
@@ -0,0 +1,25 @@
+#include <math.h>
+
+double SinIntegral(double A, double B, double e) {
+    double sum = 0.0;
+    double x = A;
+    
+    while (x < B) {
+        sum += (sin(x) + sin(x + e)) / 2.0 * e;
+        x += e;
+    }
+
+    return sum;
+}
+
+double E(double x) {
+    double sum = 1.0;
+    double fact = 1.0;
+
+    for (int n = 1; n <= x; ++n) {
+        fact *= n;
+        sum += 1.0 / fact;
+    }
+
+    return sum;
+}
